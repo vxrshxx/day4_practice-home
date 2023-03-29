2b0f9669c8c8ac8bb894e5cb69078e88e6686346
vxrshxx <124232595+vxrshxx@users.noreply.github.com>
2023-03-29 09:10:09 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Dialoguebox from './Dialoguebox'
+
+
+
+
+function App() {
+  return (
+	<div>
+		<Dialoguebox></Dialoguebox>
+	</div>
+  )
+}
+
+export default App
+
+      
+
+
+
